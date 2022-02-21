e47f8270d9bcdeecffe849274c3638be7fb65b95
BatmanofZuhandArrgh <anhnguyen199819981998@gmail.com>
2022-02-20 23:22:30 -0500

    start assignment 2

--- Assignment1_HybridSort.pl
+++ Assignment1_HybridSort.pl
@@ -10,10 +10,10 @@ hybridSort(LIST,SMALL,BIG,THRESHOLD,SLIST, ORDER).
 decreasing(X, Y):- Y =< X. 
 increasing(X, Y):- X =< Y.
 
-/swap the first two elements if they are not in order/ 
+/*swap the first two elements if they are not in order*/ 
 swap([X, Y|T], [Y, X | T], increasing):- 
             decreasing(X, Y).
-/swap elements in the tail/ 
+/*swap elements in the tail*/ 
 swap([H|T], [H|T1], increasing):- 
               swap(T, T1, increasing). 
 swap([X, Y|T], [Y, X | T], decreasing):- 
@@ -38,7 +38,7 @@ ordered([H1, H2|T], decreasing):-
     decreasing(H1, H2), 
     ordered([H2|T], decreasing).
 
-/predicate describing the insertion of an element E into a list after ensuring the list is ordered accordingly/
+/*predicate describing the insertion of an element E into a list after ensuring the list is ordered accordingly*/
 insert(X, [],[X], _). 
 insert(E, [H|T], [E,H|T], increasing):- 
                         ordered(T,increasing),
@@ -123,6 +123,7 @@ quickSort([H|T], LS, increasing):-
         quickSort(SMALL, S, increasing), 
         quickSort(BIG, B, increasing), 
         append(S, [H|B], LS). 
+
 quickSort([H|T], LS, decreasing):- 
         split(H, T, SMALL, BIG), 
         quickSort(SMALL, S, decreasing), 
@@ -137,10 +138,10 @@ when the length of list LIST is greater than or equal to THRESHOLD, then hybridS
 
 hybridSort(LIST, bubbleSort, BIGALG, T, SLIST, ORDER):-
     			length(LIST, N), N=<T,      
-    			bubbleSort(LIST, FILLINHERE, ORDER).
+    			bubbleSort(LIST, SLIST, ORDER).
 
 hybridSort(LIST, insertionSort, BIGALG, T, SLIST, ORDER):-
-			length(LIST, N), N=<T,
+			    length(LIST, N), N=<T,
      			insertionSort(LIST, SLIST, ORDER).
 
 hybridSort(LIST, SMALL, mergeSort, T, SLIST, ORDER):-
@@ -150,16 +151,47 @@ hybridSort(LIST, SMALL, mergeSort, T, SLIST, ORDER):-
     			hybridSort(L2, SMALL, mergeSort, T, S2, ORDER),
     			merge(S1,S2, SLIST, ORDER).
 
-hybridSort([H|T], SMALL, quickSort, T, SLIST, increasing):-
-			length(LIST, N), N>T,      
+hybridSort([H|T], SMALL, quickSort, Threshold, SLIST, increasing):-
+			length([H|T], N), N>Threshold,      
 			split(H, T, L1, L2),
-    		hybridSort(L1, SMALL, quicksort, T, S1, increasing),
-    		hybridSort(L2, SMALL, quicksort, T, S2, increasing),
+    		hybridSort(L1, SMALL, quickSort, Threshold, S1, increasing),
+    		hybridSort(L2, SMALL, quickSort, Threshold, S2, increasing),
     		append(S1, [H|S2], SLIST).
 
-hybridSort([H|T], SMALL, quickSort, T, SLIST, decreasing):-
-			length(LIST, N), N>T,      
+hybridSort([H|T], SMALL, quickSort, Threshold, SLIST, decreasing):-
+			length([H|T], N), N>Threshold,      
 			split(H, T, L1, L2),
-    		hybridSort(L1, SMALL, quicksort, T, S1, decreasing),
-    		hybridSort(L2, SMALL, quicksort, T, S2, decreasing),
+    		hybridSort(L1, SMALL, quickSort, Threshold, S1, decreasing),
+    		hybridSort(L2, SMALL, quickSort, Threshold, S2, decreasing),
     		append(S1, [H|S2], SLIST).
+
+myList([1,4,5]).
+
+:- initialization main.
+:- initialization writeln('1. bubble sort output').
+:- initialization forall(bubbleSort([48, 46, 40, 11, 31, 8, 17, 58, 53, 37, 1, 5, 15, 25, 88, 71, 18, 39, 41, 65, 44, 84, 47, 79, 99, 34, 4, 67, 14, 69, 86, 2, 91, 93, 55, 81, 42, 32, 85, 77, 49, 61, 35, 64, 13, 27, 36, 23, 7, 95, 52], Output, increasing), writeln(Output)).
+:- initialization writeln('2. insertion sort output').
+:- initialization forall(insertionSort([48, 46, 40, 11, 31, 8, 17, 58, 53, 37, 1, 5, 15, 25, 88, 71, 18, 39, 41, 65, 44, 84, 47, 79, 99, 34, 4, 67, 14, 69, 86, 2, 91, 93, 55, 81, 42, 32, 85, 77, 49, 61, 35, 64, 13, 27, 36, 23, 7, 95, 52], Output, increasing), writeln(Output)).
+:- initialization writeln('3. merge sort output').
+:- initialization forall(mergeSort([48, 46, 40, 11, 31, 8, 17, 58, 53, 37, 1, 5, 15, 25, 88, 71, 18, 39, 41, 65, 44, 84, 47, 79, 99, 34, 4, 67, 14, 69, 86, 2, 91, 93, 55, 81, 42, 32, 85, 77, 49, 61, 35, 64, 13, 27, 36, 23, 7, 95, 52], Output, increasing), writeln(Output)).
+:- initialization writeln('4. quick sort output').
+:- initialization forall(quickSort([48, 46, 40, 11, 31, 8, 17, 58, 53, 37, 1, 5, 15, 25, 88, 71, 18, 39, 41, 65, 44, 84, 47, 79, 99, 34, 4, 67, 14, 69, 86, 2, 91, 93, 55, 81, 42, 32, 85, 77, 49, 61, 35, 64, 13, 27, 36, 23, 7, 95, 52], Output, increasing), writeln(Output)).
+
+:- initialization writeln('5. hybrid sort output: bubble and merge').
+:- initialization forall(hybridSort([48, 46, 40, 11, 31, 8, 17, 58, 53, 37, 1, 5, 15, 25, 88, 71, 18, 39, 41, 65, 44, 84, 47, 79, 99, 34, 4, 67, 14, 69, 86, 2, 91, 93, 55, 81, 42, 32, 85, 77, 49, 61, 35, 64, 13, 27, 36, 23, 7, 95, 52], bubbleSort, mergeSort, 5, Output, increasing), writeln(Output)).
+
+:- initialization writeln('6. hybrid sort output: bubble and quick').
+:- initialization forall(hybridSort([48, 46, 40, 11, 31, 8, 17, 58, 53, 37, 1, 5, 15, 25, 88, 71, 18, 39, 41, 65, 44, 84, 47, 79, 99, 34, 4, 67, 14, 69, 86, 2, 91, 93, 55, 81, 42, 32, 85, 77, 49, 61, 35, 64, 13, 27, 36, 23, 7, 95, 52], bubbleSort, quickSort, 5, Output, increasing), writeln(Output)).
+
+:- initialization writeln('7. hybrid sort output: insert and merge').
+:- initialization forall(hybridSort([48, 46, 40, 11, 31, 8, 17, 58, 53, 37, 1, 5, 15, 25, 88, 71, 18, 39, 41, 65, 44, 84, 47, 79, 99, 34, 4, 67, 14, 69, 86, 2, 91, 93, 55, 81, 42, 32, 85, 77, 49, 61, 35, 64, 13, 27, 36, 23, 7, 95, 52], insertionSort, mergeSort, 5, Output, increasing), writeln(Output)).
+
+:- initialization writeln('8. hybrid sort output: insert and quick').
+:- initialization forall(hybridSort([48, 46, 40, 11, 31, 8, 17, 58, 53, 37, 1, 5, 15, 25, 88, 71, 18, 39, 41, 65, 44, 84, 47, 79, 99, 34, 4, 67, 14, 69, 86, 2, 91, 93, 55, 81, 42, 32, 85, 77, 49, 61, 35, 64, 13, 27, 36, 23, 7, 95, 52], insertionSort, quickSort, 5, Output, increasing), writeln(Output)).
+
+
+main :-
+    % sample_list = [33,  5, 36, 21, 38, 47, 12, 46, 20, 31, 37, 45,  2,  6, 41,  9, 24, 3,  7, 44, 35, 27, 16, 26, 10, 14, 43, 42,  4, 15, 48, 22, 18, 11, 17, 28, 39,  1, 32, 34, 25, 23,  0, 19,  8, 49, 40, 13, 30, 29];
+    % bubbleSort(sample_list, BubbleSort_output, increasing);
+    % write(BubbleSort_output);
+    write('Hello World\n').
---
+++ Assignment2_SimpleSearch.pl
@@ -0,0 +1,156 @@
+
+/* cities(L) holds when L is the list of cities on the road map */
+cities(["Arad", "Zerind", "Oradea", "Sibiu", "Timisoara", "Fagaras", "Rimnicu Vlicea", "Lugoj", "Mehadia",  "Drobeta", "Craiova", "Pitesti", "Bucharest", "Giurgiu", "Urziceni", "Hirsova", "Eforie", "Vaslui", "Iasi", "Neamt"]).
+
+/* from(C1,C2, D) holds true  when there is a direct link from city C1 to city C2 and the distance between them is D.  Note that the predicate from is NOT symmetric. Thus, based on the map we have */
+from("Arad", "Zerind", 75).
+from("Arad", "Sibiu", 140).
+from("Arad", "Timisoara", 118).
+from("Zerind", "Oradea", 71).
+
+from("Sibiu", "Oradea", 151).
+from("Sibiu", "Fagaras", 99).
+from("Sibiu", "Rimnicu Vlicea", 80).
+
+from("Fagaras", "Bucharest", 211).
+
+from("Rimnicu Vlicea", "Pitesti", 97).
+from("Rimnicu Vlicea", "Craiova", 146).
+
+from("Craiova", "Pitesti", 138).
+from("Bucharest", "Pitesti", 101).
+
+from("Craiova", "Drobeta", 120).
+
+from("Mehadia", "Drobeta", 75).
+from("Mehadia", "Lugoj", 70).
+from("Lugoj", "Timisoara", 111).
+
+from("Giurgiu", "Bucharest", 90).
+from("Bucharest", "Urziceni", 85).
+from("Urziceni", "Hirsova", 98).
+from("Eforie", "Hirsova", 86).
+from("Urziceni", "Vaslui", 142).
+from("Vaslui", "Iasi", 92).
+from("Iasi", "Neamt", 87).
+
+% c(C1, C2, D): symmetric version of from. Thus
+c(C1, C2, D):- from(C1, C2, D).
+c(C1, C2, D):- from(C2, C1, D).
+
+/* sld(C1, C2, D) is the estimate of the straight line distance between cities C1 and C2.  Note that if C1 and C2 are linked directly, then sld(C1, C2, D) is actually c(C1, C2, D).  Otherwise, we can use the ideas described in this assignment:
+*/
+sld(C1, C2, D):-c(C1, C2, D).  
+% From the table above, we have:
+sld("Arad", "Bucharest",366).
+sld("Bucharest", "Bucharest",0).
+sld("Craiova", "Bucharest",160).
+sld("Drobeta", "Bucharest",242).
+sld("Eforie", "Bucharest",161).
+sld("Fagaras", "Bucharest",176).
+sld("Giurgiu", "Bucharest",77).
+sld("Hirsova", "Bucharest",151).
+sld("Iasi", "Bucharest",226).
+sld("Lugoj", "Bucharest",244).
+
+sld("Mehandia", "Bucharest",241).
+sld("Neamt", "Bucharest",234).
+sld("Oradea", "Bucharest",380).
+sld("Pitesti", "Bucharest",100).
+sld("Rimnicu Vlicea", "Bucharest",193).
+sld("Sibiu", "Bucharest", 253).
+sld("Timisoara", "Bucharest", 329).
+sld("Urziceni", "Bucharest",80).
+sld("Vaslui", "Bucharest", 199).
+sld("Zerind", "Bucharest", 374).
+
+
+% Example of incomplete but otherwise working, depth-first search code
+
+% From Start node to Goal node
+% Start: determined upon call.
+% Goal: determined in a clause.
+% Utilizes the depth search mechanism of Prolog
+
+% Trivial: if X is the goal return X as the path from X to X.
+dfs(X, [X],_):-
+	goal(X),
+    !.
+
+% Stopping condition: Expand to all nodes, outputing all possible solution paths
+% else expand X by Y and find path from Y
+dfs(X, [X|Ypath], VISITED):-
+ 	connected(X, Y),
+  	negmember(Y, VISITED),
+  	dfs(Y, Ypath, [Y|VISITED]).
+
+% Is in list
+is_member(X,[X|_]).
+is_member(X,[_|T]):- is_member(X,T).
+
+% utility : negation of member
+negmember(X, [X|_]):-
+    !,fail.
+negmember(X, [_|T]):-
+    is_member(X, T),
+    !,
+    fail.
+negmember(_, _).
+
+% Undirected graph:
+connected(X, Y):- c(X, Y).
+connected(X, Y):- c(Y, X).
+
+%----------------------------------------------------------------------------
+% Example of incomplete but otherwise working, breadth-first search code
+/* The stopping rule for this condition is obtained when  the current path to be expanded starts with the goal node, that is:*/
+
+bfs([[X |T]|_PATHS], [X|T]):-
+               goal(X).
+
+bfs([PATH|TPaths], SOL):-
+          expand(PATH,  NPaths),
+          append(TPaths, NPaths, NEWPATHS),
+          bfs(FILL IN, SOL).
+ 
+expand([HPath|TPath], NPaths):-
+    findall([NEXT, HPath|TPath],
+       (connected(HPath, NEXT),negmember(NEXT, [HPath|TPath])),
+        NPaths).
+
+%We next wrap this in a predicate solve_BF as follows:
+solve_BFS(S, SOL):-
+         bfs([[S]], S1), 
+         FILL IN(S1,SOL).
+
+%----------------------------------------------------------------------------
+% Directed graph:
+%Test set for dfs
+% c(a, b).
+% c(a, h).
+% c(b, c).
+% c(a, d).
+% c(b, i).
+% c(d, e).
+
+%Test set 2
+c(a, b).
+c(a, h).
+c(b, c).
+c(b, i).
+c(c, d).
+c(d, e).
+c(d, i).
+c(e, f).
+c(f, g).
+c(f, h).
+c(f, i).
+c(g, h).
+c(h, i).
+
+% Specify the goal before querying the go predicate.
+goal(e).
+
+% query: ?-dfs(a, P, [a]).
+% query: ?-solve_BFS(a, SOL).
+
